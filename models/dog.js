20f92559038f5cbac9b1a558a16eed3abe15a198
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-02 08:41:37 -0600

    complete Dog schema, might update later

---
+++ models/dog.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const dogSchema = mongoose.Schema({
+    animalId: { type: String, unique: true, required: true },
+    name: String,
+    breed: String,
+    color: String,
+    age: String,
+    gender: String,
+    size: String,
+    interestedOwners: Number
+})
+
+const Dog = mongoose.model('Dog', dogSchema)
+module.exports = Dog
