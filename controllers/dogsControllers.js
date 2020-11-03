a67b8796d1e2d33d57e88d99a2ae4ff8292b2e9f
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-03 16:34:45 -0600

    can create Dog

--- controllers/dogsControllers.js
+++ controllers/dogsControllers.js
@@ -18,8 +18,9 @@ router.get('/:userId', (req, res) => {
             url: `https://api.petfinder.com/v2/animals?type=dog&location=${zipCode}`,
         })
         .then(response => {
-            // console.log(response.data.animals[1])
-            console.log(response.data.animals[1].contact)
+            console.log(response.data.animals[1])
+            // console.log(response.data.animals[1].contact)
+            // console.log(response.data.animals[1].photos[0])
             res.render('dogs/index.ejs', {
                 user: foundUser,
                 dogs: response.data.animals,
@@ -29,6 +30,38 @@ router.get('/:userId', (req, res) => {
             console.log('ERROR >>> ', error)
         }) 
     })
+})
+
+// CREATE DOG
+router.post('/:animalId', (req, res) => {
+    let animalId = req.params.animalId
+    axios({
+        method: 'get',
+        headers: { Authorization: `Bearer ${token}` },
+        url: `https://api.petfinder.com/v2/animals/${animalId}`,
+    })
+    .then(response => {
+        console.log('response received')
+        console.log(response.data.animal)
+        let dog = response.data.animal
+        let newDog = {
+            animalId: dog.id,
+            name: dog.name,
+            breed: dog.breeds.primary,
+            color: dog.colors.primary,
+            age: dog.age,
+            gender: dog.gender,
+            size: dog.size,
+            interestedOwners: 1,
+        }
+        Dog.create(newDog, (error, createdDog) => {
+            if(error) res.send(error)
+            res.send(createdDog)
+        })
+    })
+    .catch((error) => {
+        console.log('ERROR >>> ', error)
+    }) 
 })
 
 
