79fd405d2183e7d6ccab6107810e19c421f594b7
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-11-03 21:21:09 -0600

    completed adding favorite dogs to users list

--- controllers/dogsControllers.js
+++ controllers/dogsControllers.js
@@ -11,7 +11,8 @@ const token = process.env.TOKEN
 // INDEX ROUTE - SHOW ALL DOGS
 router.get('/:userId', (req, res) => {
     User.findById(req.params.userId, (error, foundUser) => {
-        let zipCode = parseInt(foundUser.zipCode)
+        let zipCode = foundUser.zipCode
+        console.log(typeof(zipCode))
         axios({
             method: 'get',
             headers: { Authorization: `Bearer ${token}` },
@@ -27,7 +28,9 @@ router.get('/:userId', (req, res) => {
             })
         })
         .catch((error) => {
-            console.log('ERROR >>> ', error)
+            console.log('ERROR >>> ', error.response.data)
+            console.log(typeof(zipCode))
+            console.log(zipCode)
         }) 
     })
 })
@@ -36,43 +39,75 @@ router.get('/:userId', (req, res) => {
 router.post('/:userId/:animalId', (req, res) => {
     let animalId = req.params.animalId
     let userId = req.params.userId
-    axios({
-        method: 'get',
-        headers: { Authorization: `Bearer ${token}` },
-        url: `https://api.petfinder.com/v2/animals/${animalId}`,
-    })
-    .then(response => {
-        console.log('response received')
-        console.log(response.data.animal)
-        let dog = response.data.animal
-        let newDog = {
-            animalId: dog.id,
-            name: dog.name,
-            breed: dog.breeds.primary,
-            color: dog.colors.primary,
-            age: dog.age,
-            gender: dog.gender,
-            size: dog.size,
-            interestedOwners: 1,
-        }
-        // create dog
-        Dog.create(newDog, (error, createdDog) => {
-            if(error) res.send(error)
-            res.send(createdDog)
+    //let foundUser = await User.findById(userId)
 
-            //Find user and add Dog
+    // Check if Animal Id is already in Database
+    Dog.find({animalId: animalId}, (error, foundDog) => {
+        console.log(foundDog)
+        if(error) {
+            console.log('Error!', error)
+            res.send(error)
+        }
+        //If the dog is not in the DB, find it in the api, add it to the DB and update the user object
+        else if(!foundDog.length){
+            console.log('api about to be called')
+            axios({
+                method: 'get',
+                headers: { Authorization: `Bearer ${token}` },
+                url: `https://api.petfinder.com/v2/animals/${animalId}`,
+            })
+            .then(response => {
+                console.log('response received')
+                console.log(response.data.animal)
+                let dog = response.data.animal
+                let newDog = {
+                    animalId: dog.id,
+                    name: dog.name,
+                    breed: dog.breeds.primary,
+                    color: dog.colors.primary,
+                    age: dog.age,
+                    gender: dog.gender,
+                    size: dog.size,
+                    interestedOwners: 1,
+                }
+                // create dog
+                Dog.create(newDog, (error, createdDog) => {
+                    if(error) res.send(error)
+        
+                    //Find user and add Dog
+                    User.findById(userId, (error, foundUser) => {
+                        foundUser.favoriteDogs.push(createdDog)
+                        foundUser.save(function (error, savedUser) {
+                            if(error) console.log(error)
+                            console.log('Saved User', savedUser)
+                        })
+                    })
+                })
+                res.redirect(`/dogs/${userId}`)
+            })
+            .catch((error) => {
+                console.log('ERROR >>> ', error)
+            }) 
+        }
+        //If Dog is found in DB, add it as a reference to User Object
+        else{
+            console.log('in uncoded function')
             User.findById(userId, (error, foundUser) => {
-                foundUser.favoriteDogs.push(createdDog)
+                if(error){
+                    console.log('EXISTING DOG ERROR >>> ', error)
+                }
+                foundUser.favoriteDogs.push(foundDog[0])
                 foundUser.save(function (error, savedUser) {
                     if(error) console.log(error)
                     console.log('Saved User', savedUser)
                 })
             })
-        })    
+            res.redirect(`/dogs/${userId}`)
+        }
     })
-    .catch((error) => {
-        console.log('ERROR >>> ', error)
-    }) 
+
+    // Find Dog from API, add to the DB, then add as reference to the user
+    
 })
 
 
